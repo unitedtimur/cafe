baabf3148239102f688b9838f3568e63d033ff72
AwesoMeme <belkadean@mail.ru>
2021-09-18 17:49:11 +0700

    declare Emilia model

---
+++ main-backend/models/character.js
@@ -0,0 +1,9 @@
+
+
+const Emilia = {
+    id: "9955266f-c845-4c11-b462-23a8b8619f83",
+    name: "Emilia",
+    workplace: "pet store",
+}
+
+module.exports = Emilia;
--- main-backend/routes/example.js
+++ main-backend/routes/example.js
@@ -1,8 +1,10 @@
+const {Emilia} = require('../models/character');
+
 const {Router} = require('express');
 const router = Router();
 
 router.get("/", (req, res) => {
-    res.json({ message: "Example route here!" });
+    res.json(Emilia.id);
 });
 
 module.exports = router;
