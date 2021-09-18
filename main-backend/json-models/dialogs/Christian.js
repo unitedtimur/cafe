3331b0590c8e1851596fee7fbee46c0e7b14042a
nblaaa <nblaaa@mail.ru>
2021-09-18 22:13:57 +0700

    Created js files with dialogs.

---
+++ main-backend/json-models/dialogs/Christian.js
@@ -0,0 +1,163 @@
+const ChristianDialogs = [
+	{
+		"id": 1,
+		"characterId": "1c2a8701-f00b-4359-9d3f-ae18534a38d9",
+		"isPlayersPhrase": false,
+		"isChoise": false,
+		"phrase": "Добрый день.",
+		"nextId": 2
+	},
+
+	{
+		"id": 2,
+		"characterId": null,
+		"isPlayersPhrase": true,
+		"isChoise": true,
+		"phrasesArray": ["Здравствуйте. Расскажете мне новую историю про какое-нибудь интересное дело, которые вы когда-то раскрыли?", "<Промолчать>"],
+		"nextIdsArray": [3, null]
+	},
+
+	{
+		"id": 3,
+		"characterId": "1c2a8701-f00b-4359-9d3f-ae18534a38d9",
+		"isPlayersPhrase": false,
+		"isChoise": false,
+		"phrase": "Лучше рассказать про таинственное зеркало? Или про то, как мы ловили преступника?",
+		"nextId": 4
+	},
+
+	{
+		"id": 4,
+		"characterId": null,
+		"isPlayersPhrase": true,
+		"isChoise": true,
+		"phrasesArray": ["Про зеркало.", "Про преступника."],
+		"nextIdsArray": [5, 13]
+	},
+
+	{
+		"id": 5,
+		"characterId": "1c2a8701-f00b-4359-9d3f-ae18534a38d9",
+		"isPlayersPhrase": false,
+		"isChoise": false,
+		"phrase": "Если угостите меня.",
+		"nextId": 6
+	},
+
+	{
+		"id": 6,
+		"characterId": null,
+		"isPlayersPhrase": true,
+		"isChoise": true,
+		"phrasesArray": ["<Угостить>", "<Промолчать>"],
+		"nextIdsArray": [7, null]
+	},
+
+	{
+		"id": 7,
+		"characterId": "1c2a8701-f00b-4359-9d3f-ae18534a38d9",
+		"isPlayersPhrase": false,
+		"isChoise": false,
+		"phrase": "Когда-то давно я расследовал очень интересную историю, связанную со старинным зеркалом, которое стояло в музее искусств.",
+		"nextId": 8
+	},
+
+	{
+		"id": 8,
+		"characterId": "1c2a8701-f00b-4359-9d3f-ae18534a38d9",
+		"isPlayersPhrase": false,
+		"isChoise": false,
+		"phrase": "По словам очевидцев, каждый вечер в зеркале отображались жёлтые страшные глаза.",
+		"nextId": 9
+	},
+
+	{
+		"id": 9,
+		"characterId": "1c2a8701-f00b-4359-9d3f-ae18534a38d9",
+		"isPlayersPhrase": false,
+		"isChoise": false,
+		"phrase": "Понаблюдав за всем этим делом, мы выяснили, что в музей пробрался котёнок, который каждый раз своими яркими глазами пугал охранников.",
+		"nextId": 10
+	},
+
+	{
+		"id": 10,
+		"characterId": "1c2a8701-f00b-4359-9d3f-ae18534a38d9",
+		"isPlayersPhrase": false,
+		"isChoise": false,
+		"phrase": "Так и закончилось моё расследование таинственного зеркала.",
+		"nextId": 11
+	},
+
+	{
+		"id": 11,
+		"characterId": null,
+		"isPlayersPhrase": true,
+		"isChoise": false,
+		"phrase": "Ого, как интересно!",
+		"nextId": 12
+	},
+
+	{
+		"id": 12,
+		"characterId": "1c2a8701-f00b-4359-9d3f-ae18534a38d9",
+		"isPlayersPhrase": false,
+		"isChoise": false,
+		"phrase": "Спасибо вам за угощение. До встречи.",
+		"nextId": null
+	},
+
+	{
+		"id": 13,
+		"characterId": "1c2a8701-f00b-4359-9d3f-ae18534a38d9",
+		"isPlayersPhrase": false,
+		"isChoise": false,
+		"phrase": "Относительно недавно мы расследовали дело о проникновении на частную собственность.",
+		"nextId": 14
+	},
+
+	{
+		"id": 14,
+		"characterId": "1c2a8701-f00b-4359-9d3f-ae18534a38d9",
+		"isPlayersPhrase": false,
+		"isChoise": false,
+		"phrase": "Однажды ночью кто-то позвонил в панике и сказал, что охранная система сигнализирует о том, что кто-то пробрался на частную собственность.",
+		"nextId": 15
+	},
+
+	{
+		"id": 15,
+		"characterId": "1c2a8701-f00b-4359-9d3f-ae18534a38d9",
+		"isPlayersPhrase": false,
+		"isChoise": false,
+		"phrase": "Приехав по адресу, мы увидели много паутины на датчиках, из-за чего сигнализация и играла. Вот такая вот история.",
+		"nextId": 16
+	},
+
+	{
+		"id": 16,
+		"characterId": null,
+		"isPlayersPhrase": true,
+		"isChoise": false,
+		"phrase": "Хах, какая поучительная история.",
+		"nextId": 17
+	},
+
+	{
+		"id": 17,
+		"characterId": "1c2a8701-f00b-4359-9d3f-ae18534a38d9",
+		"isPlayersPhrase": false,
+		"isChoise": false,
+		"phrase": "Именно так. Поэтому следите за вашим кафе, оно мне очень нравится.",
+		"nextId": 18
+	},
+
+	{
+		"id": 18,
+		"characterId": "1c2a8701-f00b-4359-9d3f-ae18534a38d9",
+		"isPlayersPhrase": false,
+		"isChoise": false,
+		"phrase": "Спасибо за угощение. Ещё увидимся.",
+		"nextId": null
+	}
+] 
