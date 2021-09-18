f2a2033ec9582c8910c2604489137ca22fea50c1
AwesoMeme <belkadean@mail.ru>
2021-09-18 15:44:24 +0700

    Set up server

---
+++ main-backend/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const exampleRoute = require("./routes/example");
+
+const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+//db set up
+const db = require("./models");
+db.sequelize.sync();
+
+// route registration
+app.use('/', exampleRoute);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
