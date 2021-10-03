813964b64d1608dab28b04a57879c1ade34bc615
nblaaa <nblaaa@mail.ru>
2021-10-03 17:50:50 +0700

    Megred playerPhrasesArray and nextIdsArray into one array.

--- main-backend/json-models/dialogs/Emilia.js
+++ main-backend/json-models/dialogs/Emilia.js
@@ -3,55 +3,89 @@ const EmiliaDialogs = [
 		"dialogId": "6ff49fbb-c801-41dd-8d82-ac59e5ca939a",
 		"characterId": "9955266f-c845-4c11-b462-23a8b8619f83",
 		"phrase": "Привет!",
-		"playerPhrasesArray": ["Привет, Эмилия. Как дела?", "<Промолчать>"],
-		"nextIdsArray": ["0f6e2d58-b698-4128-a4d7-7f4669cfa6e4", null]
+		"playerPhrasesArray": [
+			{
+				"nextDialogId": "0f6e2d58-b698-4128-a4d7-7f4669cfa6e4",
+				"phrase": "Привет, Эмилия. Как дела?"
+			},
+			{
+				"nextDialogId": null,
+				"phrase": "<Промолчать>"
+			}
+		]
 	},
 
 	{
 		"dialogId": "0f6e2d58-b698-4128-a4d7-7f4669cfa6e4",
 		"characterId": "9955266f-c845-4c11-b462-23a8b8619f83",
 		"phrase": "Представляешь, нам сегодня привезли новых животных!",
-		"playerPhrasesArray": ["Ого, расскажешь про них подробнее?", "Тебе правда это интересно?"],
-		"nextIdsArray": ["63fceea7-7586-4cda-9e14-3346b09ef982", "661ceb23-86b6-45a5-863e-ee10eeec8cf9"]
+		"playerPhrasesArray": [
+			{
+				"nextDialogId": "63fceea7-7586-4cda-9e14-3346b09ef982",
+				"phrase": "Ого, расскажешь про них подробнее?"
+			},
+			{
+				"nextDialogId": "661ceb23-86b6-45a5-863e-ee10eeec8cf9",
+				"phrase": "Тебе правда это интересно?"
+			}
+		]
 	},
 
 	{
 		"dialogId": "63fceea7-7586-4cda-9e14-3346b09ef982",
 		"characterId": "9955266f-c845-4c11-b462-23a8b8619f83",
 		"phrase": "Да, если ты угостишь меня.",
-		"playerPhrasesArray": ["<Угостить>", "<Промолчать>"],
-		"nextIdsArray": ["97096186-1a72-4e0f-809d-d7c6626f4bf9", null]
+		"playerPhrasesArray": [
+			{
+				"nextDialogId": "97096186-1a72-4e0f-809d-d7c6626f4bf9",
+				"phrase": "<Угостить>"
+			},
+			{
+				"nextDialogId": null,
+				"phrase": "<Промолчать>"
+			}
+		]
 	},
 
 	{
 		"dialogId": "97096186-1a72-4e0f-809d-d7c6626f4bf9",
 		"characterId": "9955266f-c845-4c11-b462-23a8b8619f83",
 		"phrase": "Сегодня нам в зоомагазин привезли маленьких морских черепашек, представляешь? Они такие маленькие, я прям не могу. Спасибо за угощение.",
-		"playerPhrasesArray": null,
-		"nextIdsArray": null
+		"playerPhrasesArray": null
 	},
 
 	{
 		"dialogId": "661ceb23-86b6-45a5-863e-ee10eeec8cf9",
 		"characterId": "9955266f-c845-4c11-b462-23a8b8619f83",
 		"phrase": "Ну да, а что в этом такого?",
-		"playerPhrasesArray": ["Нет, ничего. Расскажешь про них подробнее?"],
-		"nextIdsArray": ["e9ccc511-d2c1-4da3-9f7f-0c672df1344c"]
+		"playerPhrasesArray": [
+			{
+				"nextDialogId": "e9ccc511-d2c1-4da3-9f7f-0c672df1344c",
+				"phrase": "Нет, ничего. Расскажешь про них подробнее?"
+			}
+		]
 	},
 
 	{
 		"dialogId": "e9ccc511-d2c1-4da3-9f7f-0c672df1344c",
 		"characterId": "9955266f-c845-4c11-b462-23a8b8619f83",
 		"phrase": "Да, если ты угостишь меня.",
-		"playerPhrasesArray": ["<Угостить>", "<Промолчать>"],
-		"nextIdsArray": ["2022090b-fd20-4045-b39d-0e36707fd729", null]
+		"playerPhrasesArray": [
+			{
+				"nextDialogId": "2022090b-fd20-4045-b39d-0e36707fd729",
+				"phrase": "<Угостить>"
+			},
+			{
+				"nextDialogId": null,
+				"phrase": "<Промолчать>"
+			}
+		]
 	},
 
 	{
 		"dialogId": "2022090b-fd20-4045-b39d-0e36707fd729",
 		"characterId": "9955266f-c845-4c11-b462-23a8b8619f83",
 		"phrase": "Сегодня нам в зоомагазин привезли маленьких морских черепашек, представляешь? Они такие маленькие, я прям не могу. Спасибо за угощение.",
-		"playerPhrasesArray": null,
-		"nextIdsArray": null
+		"playerPhrasesArray": null
 	}
 ]
